70177b2778a539b47b734595a6e4d427707ba53e
sandy2108 <dhonisanjay116@gmail.com>
2023-06-29 01:11:03 +0530

    text color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
           </Link>
           <Link
             href="tel:021 023 26040"
-            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
           >
             Contact us
           </Link>
